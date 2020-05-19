1aca7a37bf3c66a366d7c016b761a92545f5711b
William Marin <williammarinalvarado@gmail.com>
2020-05-19 18:37:44 -0500

    Modificaciones en los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
-import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PagesComponent } from './pages/pages.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
@@ -12,23 +8,20 @@ import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component
 // Cuando tengamos el login y el registro por aparte del template principal se deben agregar hijo "Childrens" para poder manejarlos por separado
 
 const routes: Routes = [
-  { 
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graficas1', component: Graficas1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-    ]
-  },
+  // { 
+  //   path: '',
+  //   component: PagesComponent,
+  //   children: [
+  //     { path: 'dashboard', component: DashboardComponent },
+  //     { path: 'progress', component: ProgressComponent },
+  //     { path: 'graficas1', component: Graficas1Component },
+  //     { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  //   ]
+  // },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', component: NopagefoundComponent },
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true})],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+
+export const APP_ROUTES = RouterModule.forRoot(routes, { useHash: true});
