723677acc3ecd00bce06cc937ad098069ee75cbb
William Marin <williammarinalvarado@gmail.com>
2020-05-21 21:15:15 -0500

    Fin de la seccion 7

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -11,6 +11,7 @@ import { PAGES_ROUTES } from './pages.routes';
 // temporal
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 
@@ -21,7 +22,8 @@ import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.co
         ProgressComponent,
         Graficas1Component,
         IncrementadorComponent,
-        GraficoDonaComponent
+        GraficoDonaComponent,
+        AccountSettingsComponent
     ],
     exports: [
         DashboardComponent,
