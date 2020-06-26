f94fc5284b0a2a48dd8c1f37c2091d04af4052e4
William Marin <williammarinalvarado@gmail.com>
2020-06-25 22:10:39 -0500

    Sección 14 lista

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule} from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
+import { CommonModule } from '@angular/common';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { PAGES_ROUTES } from './pages.routes';
+
+// Pipe Modulo
+import { PipesModule } from '../pipes/pipes.module';
 
 // temporal
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
@@ -14,6 +19,7 @@ import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.co
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
@@ -27,7 +33,8 @@ import { RxjsComponent } from './rxjs/rxjs.component';
         GraficoDonaComponent,
         AccountSettingsComponent,
         PromesasComponent,
-        RxjsComponent
+        RxjsComponent,
+        ProfileComponent
     ],
     exports: [
         DashboardComponent,
@@ -35,10 +42,12 @@ import { RxjsComponent } from './rxjs/rxjs.component';
         Graficas1Component
     ],
     imports: [
+        CommonModule,
         SharedModule,
         PAGES_ROUTES,
         FormsModule,
-        ChartsModule
+        ChartsModule,
+        PipesModule
     ]
 })
 export class PagesModule { }
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -6,6 +6,7 @@ import { URL_SERVICIOS } from '../../config/config';
 import { map } from 'rxjs/operators/';
 import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
+import { SubirArchivoService } from '../subir-archivo/subir-archivo.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,8 @@ export class UsuarioService {
   token: string;
 
   constructor(public http: HttpClient,
-              public router: Router) {
+              public router: Router,
+              public _subirArchivoService: SubirArchivoService) {
     this.cargarStorage();
   }
 
@@ -84,6 +86,34 @@ export class UsuarioService {
                 Swal.fire('Usuario creado', usuario.email, 'success');
                 return resp.usuario;
     }));
+  }
+
+  actualizarUsuario(usuario: Usuario){
+
+    let url = URL_SERVICIOS + '/usuario/' + usuario._id;
+    url += '?token=' + this.token;
+
+    
+    return this.http.put( url, usuario).pipe(map( (resp: any) => {
+      let usuarioDB: Usuario = resp.usuario;
+      this.guardarStorage(usuarioDB._id, this.token, usuarioDB);
+      Swal.fire('Usuario actualizado', usuario.nombre, 'success');
+      return true;
+    }));
+  }
+
+  cambiarImagen( archivo: File, id: string){
+    this._subirArchivoService.subirArchivo( archivo, 'usuarios', id)
+        .then((resp: any) => {
+          console.log(resp);
+          this.usuario.img = resp.usuario.img;
+          Swal.fire('Imagen actualizado', this.usuario.nombre, 'success');
+
+          this.guardarStorage(id, this.token, this.usuario);
+        })
+        .catch( resp => {
+          console.log(resp);
+        });
   }
    
 }
